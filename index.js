c76aceb49888d03405c743fa412d9c9cc36139d6
dineshkumar-stack <stardinesh4@gmail.com>
2023-07-14 21:51:45 +0530

    nodeb2.2

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const dirPath = path.join(__dirname, "timeStamp");
+
+const app = express()
+app.get("/timestamp", (req, res) => {
+    let date = new Date();
+    const timeStampDate = date.toUTCString().slice(0, -3);
+    fs.writeFileSync(`${path.join(dirPath, "current date-time.txt")}`, timeStampDate, (err) => {
+        if (err) {
+            return res.sent({ message: "error timestamp" })
+        }
+    })
+    res.sendFile(path.join(dirPath, "current date-time.txt"))
+})
+app.listen(9000, () => console.log(`Server started in 9000`))
+
+
+
